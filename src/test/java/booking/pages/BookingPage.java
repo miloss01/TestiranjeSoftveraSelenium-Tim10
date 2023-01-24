2f3c6ccd0f9d3f378b22097f75c7814914e377cd
SelenaMilutin <selena.milutin@gmail.com>
2023-01-24 19:56:49 +0100

    Add test logic

--- src/test/java/booking/pages/BookingPage.java
+++ src/test/java/booking/pages/BookingPage.java
@@ -1,11 +1,19 @@
 package booking.pages;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.security.Key;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class BookingPage {
     private WebDriver driver;
@@ -38,17 +46,27 @@ public class BookingPage {
 //    @FindBy(tagName = "input")
 //    WebElement friendInputField;
 
-    @FindBy(xpath = "//input[placeholder=\"Book time\"]")
+    @FindBy(css = "input[placeholder=\"Book time\"]")
     WebElement datePicker;
 
-    @FindBy(xpath = "//p[contains(., \"Pets\")]/mat-checkbox")
+    @FindBy(id="pet-checkbox")
     WebElement petCheckBox;
 
     @FindBy(xpath = "//p[contains(., \"Kids\")]/mat-checkbox")
     WebElement babyCheckBox;
 
+    @FindBy(xpath = "//p[starts-with(., \"Estimated price:\")]")
+    WebElement estimatedPriceLabel;
+
     @FindBy(id = "book-btn")
     WebElement bookRideButton;
+
+
+    @FindBy(xpath = "//button[contains(., \"OK\") and contains(@class, \"swal2-confirm\")]")
+    WebElement swalOkButton;
+
+    @FindBy(xpath = "//button[contains(., \"OK!\") and contains(@class, \"swal2-confirm\")]")
+    WebElement swalOkButtonRideAccepted;
 
     public BookingPage(WebDriver driver) {
         this.driver = driver;
@@ -63,16 +81,127 @@ public class BookingPage {
     }
 
     public void fillDepartureTextBox(String address) {
+        (new WebDriverWait(driver, 3))
+                .until(ExpectedConditions.elementToBeClickable(departureSearchField));
         departureSearchField.clear();
         departureSearchField.sendKeys(address);
     }
 
     public void fillDestinationTextBox(String address) {
+        (new WebDriverWait(driver, 3))
+                .until(ExpectedConditions.elementToBeClickable(destinationSearchField));
         destinationSearchField.clear();
         destinationSearchField.sendKeys(address);
     }
 
     public void clickOnMap(int xOffset, int yOffset) {
 
+    }
+
+    public void clickEstimate() {
+        (new WebDriverWait(driver, 3))
+                .until(ExpectedConditions.elementToBeClickable(estimateButton)).click();
+        System.out.println(estimatedPriceLabel.getText());
+        (new WebDriverWait(driver, 7))
+                .until(ExpectedConditions.textToBePresentInElement(estimatedPriceLabel, "Estimated price: $370"));
+        System.out.println(estimatedPriceLabel.getText());
+
+    }
+
+    public void clickBook() {
+        (new WebDriverWait(driver, 3))
+                .until(ExpectedConditions.elementToBeClickable(bookRideButton)).click();
+    }
+
+    public void clickPetCheckbox() {
+        (new WebDriverWait(driver, 3))
+                .until(ExpectedConditions.elementToBeClickable(petCheckBox)).click();
+        (new WebDriverWait(driver, 3))
+                .until(ExpectedConditions.elementToBeClickable(petCheckBox));
+    }
+
+    public void selectInvalidDate() {
+        LocalDateTime date = LocalDateTime.now();
+        date = date.plusHours(5);
+        date = date.plusMinutes(10);
+        LocalDateTime dateForForm = date;
+        if (date.getHour() >= 12) dateForForm = dateForForm.minusHours(12);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MMddyyyy HHmm");
+        String dateString = dateForForm.format(formatter);
+        System.out.println(dateString);
+        (new WebDriverWait(driver, 3))
+                .until(ExpectedConditions.elementToBeClickable(datePicker));
+        for (int i = 0; i < 4; i=i+2) {
+            datePicker.sendKeys(dateString.substring(i, i + 2));
+            (new WebDriverWait(driver, 3))
+                    .until(ExpectedConditions.elementToBeClickable(datePicker));
+        }
+        datePicker.sendKeys(dateString.substring(4, 8));
+        datePicker.sendKeys(Keys.ARROW_RIGHT);
+        datePicker.sendKeys(dateString.substring(9, 11));
+        System.out.println(dateString.substring(9, 11));
+        datePicker.sendKeys(dateString.substring(11));
+
+        if (date.getHour() >= 12) datePicker.sendKeys(Keys.chord(Keys.SHIFT, "p"));
+        else datePicker.sendKeys(Keys.chord(Keys.SHIFT, "a"));
+    }
+
+    public void selectValidDate() {
+        LocalDateTime date = LocalDateTime.now();
+        date = date.plusMinutes(10);
+        LocalDateTime dateForForm = date;
+        if (date.getHour() >= 12) dateForForm = dateForForm.minusHours(12);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MMddyyyy HHmm");
+        String dateString = dateForForm.format(formatter);
+        System.out.println(dateString);
+        (new WebDriverWait(driver, 3))
+                .until(ExpectedConditions.elementToBeClickable(datePicker));
+        for (int i = 0; i < 4; i=i+2) {
+            datePicker.sendKeys(dateString.substring(i, i + 2));
+            (new WebDriverWait(driver, 3))
+                    .until(ExpectedConditions.elementToBeClickable(datePicker));
+        }
+        datePicker.sendKeys(dateString.substring(4, 8));
+        datePicker.sendKeys(Keys.ARROW_RIGHT);
+        datePicker.sendKeys(dateString.substring(9, 11));
+        System.out.println(dateString.substring(9, 11));
+        datePicker.sendKeys(dateString.substring(11));
+
+        if (date.getHour() >= 12) datePicker.sendKeys(Keys.chord(Keys.SHIFT, "p"));
+        else datePicker.sendKeys(Keys.chord(Keys.SHIFT, "a"));
+    }
+
+    public void selectDropDown(String vehicleType) {
+        (new WebDriverWait(driver, 3))
+                .until(ExpectedConditions.elementToBeClickable(vehicleTypeDropdown)).click();
+//        Select select = new Select(vehicleTypeDropdown);
+//        select.selectByVisibleText(vehicleType);
+        driver.findElement(By.xpath("//mat-option[contains(., \""+ vehicleType + "\")]")).click();
+        (new WebDriverWait(driver, 3))
+                .until(ExpectedConditions.elementToBeClickable(vehicleTypeDropdown));
+    }
+
+    public void reloadPage() {
+        driver.navigate().to(PAGE_URL);
+        loadPage();
+    }
+
+    public void exitSwalPopUp() {
+        (new WebDriverWait(driver, 3))
+                .until(ExpectedConditions.visibilityOf(swalOkButton));
+        (new WebDriverWait(driver, 3))
+                .until(ExpectedConditions.elementToBeClickable(swalOkButton)).click();
+    }
+
+    public void exitSwalPopUp2() {
+        (new WebDriverWait(driver, 3))
+                .until(ExpectedConditions.visibilityOf(swalOkButtonRideAccepted));
+        (new WebDriverWait(driver, 3))
+                .until(ExpectedConditions.elementToBeClickable(swalOkButtonRideAccepted)).click();
+    }
+
+    public void waitForPopUp() {
+        (new WebDriverWait(driver, 3))
+                .until(ExpectedConditions.stalenessOf(swalOkButton));
     }
 }
--- src/test/java/booking/tests/BookRideTest.java
+++ src/test/java/booking/tests/BookRideTest.java
@@ -7,39 +7,72 @@ import org.testng.annotations.Test;
 import static helper.Helper.takeScreenshoot;
 
 public class BookRideTest extends TestBaseForBooking {
-    static final String DEPARTURE_ADDRESS = "Developer";
-    static final String DESTINATION_ADDRESS = "email";
+    static final String DEPARTURE_ADDRESS = "Vuka Karadžića 6, Novi Sad";
+    static final String DESTINATION_ADDRESS = "Pavleka Miškine 17, Novi Sad";
     static final String FRIEND_NAME = "full name";
-    static final String VEHICLE_TYPE = "luxury";
+    static final String VEHICLE_TYPE = "Luxury";
 
-    @Test
+    @Test(priority=1)
     public void bookRideWithNoAddress() {
         BookingPage bookingPage = new BookingPage(driver);
         Assert.assertTrue(bookingPage.loadPage());
-        takeScreenshoot(driver, "jj");
+        bookingPage.clickBook();
+        takeScreenshoot(driver, "booking_address_not_filled");
     }
 
-//    @Test
-//    public void bookRideInvalidDate() {
-//        BookingPage bookingPage = new BookingPage(driver);
-//
-//    }
-//
-//    @Test
-//    public void bookRideNoAvailableVehicles() {
-//        BookingPage bookingPage = new BookingPage(driver);
-//
-//    }
-//
-//    @Test
-//    public void bookRideHappyEnding() {
-//        BookingPage bookingPage = new BookingPage(driver);
-//
-//    }
-//
-//    @Test
-//    public void bookRideAlreadyPending() {
-//        BookingPage bookingPage = new BookingPage(driver);
-//
-//    }
+    @Test(priority=2)
+    public void bookRideInvalidDate() {
+        BookingPage bookingPage = new BookingPage(driver);
+        bookingPage.fillDepartureTextBox(DEPARTURE_ADDRESS);
+        bookingPage.fillDestinationTextBox(DESTINATION_ADDRESS);
+        bookingPage.clickEstimate();
+        bookingPage.clickPetCheckbox();
+        bookingPage.clickBook();
+        takeScreenshoot(driver, "booking_time_not_selected");
+        bookingPage.clickEstimate();
+        bookingPage.selectInvalidDate();
+        bookingPage.clickBook();
+        takeScreenshoot(driver, "booking_time_not_valid");
+    }
+
+    @Test(priority = 3)
+    public void bookRideNoAvailableVehicles() {
+        BookingPage bookingPage = new BookingPage(driver);
+        bookingPage.reloadPage();
+        bookingPage.fillDepartureTextBox(DEPARTURE_ADDRESS);
+        bookingPage.fillDestinationTextBox(DESTINATION_ADDRESS);
+        bookingPage.clickEstimate();
+        bookingPage.selectValidDate();
+        bookingPage.clickBook();
+//        bookingPage.waitForPopUp();
+        takeScreenshoot(driver, "booking_no_vehicles");
+    }
+
+    @Test(priority = 4)
+    public void bookRideHappyEnding() {
+        BookingPage bookingPage = new BookingPage(driver);
+        bookingPage.exitSwalPopUp();
+        bookingPage.reloadPage();
+        bookingPage.fillDepartureTextBox(DEPARTURE_ADDRESS);
+        bookingPage.fillDestinationTextBox(DESTINATION_ADDRESS);
+        bookingPage.clickEstimate();
+        bookingPage.selectDropDown(VEHICLE_TYPE);
+        bookingPage.selectValidDate();
+        bookingPage.clickBook();
+        takeScreenshoot(driver, "booking_successful");
+    }
+
+    @Test(priority = 5)
+    public void bookRideAlreadyPending() {
+        BookingPage bookingPage = new BookingPage(driver);
+        takeScreenshoot(driver, "booking_no_vehicles");
+        bookingPage.reloadPage();
+        bookingPage.fillDepartureTextBox(DEPARTURE_ADDRESS);
+        bookingPage.fillDestinationTextBox(DESTINATION_ADDRESS);
+        bookingPage.clickEstimate();
+        bookingPage.selectDropDown(VEHICLE_TYPE);
+        bookingPage.selectValidDate();
+        bookingPage.clickBook();
+        takeScreenshoot(driver, "booking_ride_already_pending");
+    }
 }
